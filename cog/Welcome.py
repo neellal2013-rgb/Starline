18ee71d8e15151453276cd5dca1a6ff731ece763
neellal2013-rgb <neellal2013@gmail.com>
2026-08-10 14:43:33 +0530

    Fix welcome message formatting in Welcome.py

--- cog/Welcome.py
+++ cog/Welcome.py
@@ -17,7 +17,7 @@ class Welcome(commands.Cog):
 
         if channel:
             description = (
-                f"THANK YOU FOR JOINING STARLINE CAFE
+                f"THANK YOU FOR JOINING STARLINE CAFE"
                 f"{member.mention} thanks for joining server\n"
                 f"visit these channels\n"
                 f"{ARROW_EMOJI} Checkout - <#922323143928995911>\n"
